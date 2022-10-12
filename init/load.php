f6a5ac7c51f1c009dacf041db9d98bdfc659814e
Rik Janssen <56626554+rik-janssen@users.noreply.github.com>
2022-10-12 09:11:56 +0200

    Create load.php

---
+++ init/load.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Load the required
+ * routes and hooks.
+ */
+
+require_once ROOTPATH.'/vendor/vesperphp/elemental/init/routes.php';
+require_once ROOTPATH.'/vendor/vesperphp/elemental/init/hooks.php';
