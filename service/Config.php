f536b579e2964f292bae8026ec54f99369027f24
TBC <shoutatrikjanssen@gmail.com>
2022-11-24 11:55:17 +0100

    smashing

---
+++ service/Config.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Config;
+use Config\Shorthand;
+
+class Config{
+  
+  /**
+   * This method loads the config 
+   * file and stores everything 
+   * neatly in an array. With
+   * the available parameters
+   * you can fetch those items
+   * from the array.
+   */
+
+  public static function get(string $path){
+
+    /**
+     * Check if the parent and 
+     * child are available..
+     */
+
+     $config = V_CONFIG;
+ 
+    if($path) {
+
+          
+      return Shorthand::get($config, $path);
+
+    }
+    
+    return false;
+
+  }
+
+}
